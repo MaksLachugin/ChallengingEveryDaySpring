7d08c718713394194a74d7450684870df54240c3
Lachugin <lachugin@cs.vsu.ru>
2022-04-26 23:18:21 +0300

    2022.04.26.006

--- src/main/java/ru/vsu/cs/lachugin/dao/ButtonDAO.java
+++ src/main/java/ru/vsu/cs/lachugin/dao/ButtonDAO.java
@@ -1,4 +1,45 @@
 package ru.vsu.cs.lachugin.dao;
 
+import ru.vsu.cs.lachugin.models.Button;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ButtonDAO {
+    private static final String URL = "";
+    private static final String USERNAME = "";
+    private static final String PASS = "";
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Button> index() {
+        return null;
+    }
+
+    public Button show(int id) {
+        return null;
+    }
+
+    public void save(Button person) {
+    }
+
+    public void update(int id, Button updatedPerson) {
+    }
+
+    public void delete(int id) {
+    }
 }
